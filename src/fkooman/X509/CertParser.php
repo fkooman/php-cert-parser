1a17ce65500fafe3fef1f05d5e93cefea27c500e
François Kooman <fkooman@tuxed.net>
2013-05-24 13:54:52 +0200

    refactoring

--- src/fkooman/X509/CertParser.php
+++ src/fkooman/X509/CertParser.php
@@ -5,8 +5,8 @@ namespace fkooman\X509;
 class CertParser
 {
 
-    private $_c;
-    private $_fingerPrint;
+    private $_strippedCert;
+    private $_parsedCert;
 
     /**
      * Construct the CertParser object.
@@ -19,30 +19,44 @@ class CertParser
             throw new CertParserException("input should be string");
         }
 
+        // strip header and footer from PEM if present
+        $certData = preg_replace('/\-+BEGIN CERTIFICATE\-+/', '', $certData);
+        $certData = preg_replace('/\-+END CERTIFICATE\-+/', '', $certData);
+
+        // create one long string of the certificate
+        $replaceCharacters = array(" ", "\t", "\n", "\r", "\0" , "\x0B");
+        $certData = str_replace($replaceCharacters, '', $certData);
+
+        // store this stripped certificate
+        $this->_strippedCert = $certData;
+
+        // parse the certificate using OpenSSL
         if (!function_exists("openssl_x509_parse")) {
             throw new CertParserException("php openssl extension not available");
         }
 
-        // normalize the certificate to be able to calculate fingerprint
-        $certData = preg_replace('/\-+BEGIN CERTIFICATE\-+/', '', $certData);
-        $certData = preg_replace('/\-+END CERTIFICATE\-+/', '', $certData);
-
-        // replace all new lines and other space symbols with nothing
-        $replaceCharacters = array(" ", "\t", "\n", "\r", "\0" , "\x0B");
-        $certData = str_replace($replaceCharacters, '', $certData);
-
-        $this->_fingerPrint = sha1(base64_decode($certData));
-
-        // maximum 64 characters line length
-        $certData = wordwrap($certData, 64, "\n", TRUE);
-
-        // prepend header and append footer
-        $certData = "-----BEGIN CERTIFICATE-----" . PHP_EOL . $certData . PHP_EOL . "-----END CERTIFICATE-----" . PHP_EOL;
-
-        $this->_c = openssl_x509_parse($certData);
-        if (FALSE === $this->_c) {
+        $c = openssl_x509_parse($this->toPEM());
+        if (FALSE === $c) {
             throw new CertParserException("unable to parse the certificate");
         }
+
+        $this->_parsedCert = $c;
+    }
+
+    public function toBase64()
+    {
+        return $this->_strippedCert;
+    }
+
+    public function toDer()
+    {
+        return base64_decode($this->toBase64());
+    }
+
+    public function toPem()
+    {
+        // prepend header and append footer
+        return "-----BEGIN CERTIFICATE-----" . PHP_EOL . wordwrap($this->toBase64(), 64, "\n", TRUE) . PHP_EOL . "-----END CERTIFICATE-----" . PHP_EOL;
     }
 
     public static function fromFile($fileName)
@@ -60,11 +74,11 @@ class CertParser
      */
     public function getNotValidBefore()
     {
-        if (!array_key_exists("validFrom_time_t", $this->_c)) {
+        if (!array_key_exists("validFrom_time_t", $this->_parsedCert)) {
             throw new CertParserException("could not find 'validFrom_time_t' key");
         }
 
-        return $this->_c['validFrom_time_t'];
+        return $this->_parsedCert['validFrom_time_t'];
     }
 
     /**
@@ -72,21 +86,16 @@ class CertParser
      */
     public function getNotValidAfter()
     {
-        if (!array_key_exists("validTo_time_t", $this->_c)) {
+        if (!array_key_exists("validTo_time_t", $this->_parsedCert)) {
             throw new CertParserException("could not find 'validTo_time_t' key");
         }
 
-        return $this->_c['validTo_time_t'];
+        return $this->_parsedCert['validTo_time_t'];
     }
 
     public function getFingerprint()
     {
-        return $this->_fingerPrint;
-    }
-
-    public function getCertificateData()
-    {
-        return $this->_c;
+        return sha1($this->toDer());
     }
 
     /**
@@ -94,11 +103,11 @@ class CertParser
      */
     public function getName()
     {
-        if (!array_key_exists("name", $this->_c)) {
+        if (!array_key_exists("name", $this->_parsedCert)) {
             throw new CertParserException("could not find 'name' key");
         }
 
-        return $this->_c['name'];
+        return $this->_parsedCert['name'];
     }
 
 }
--- tests/fkooman/X509/CertParserTest.php
+++ tests/fkooman/X509/CertParserTest.php
@@ -11,15 +11,19 @@ class ClientRegistrationTest extends PHPUnit_Framework_TestCase
 
     public function testCert()
     {
-        $dataDir = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR;
-        $testFiles = array ($dataDir . "1.pem", $dataDir . "1_flat.pem");
+        $dataDir = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . "data";
+        $testFiles = array ("1.pem");
 
         foreach ($testFiles as $t) {
-            $c = CertParser::fromFile($t);
+            $c = CertParser::fromFile($dataDir . DIRECTORY_SEPARATOR . $t);
             $this->assertEquals(1295864337, $c->getNotValidBefore());
             $this->assertEquals(1611397137, $c->getNotValidAfter());
             $this->assertEquals('/C=NL/ST=Utrecht/L=Utrecht/O=SURFnet B.V./OU=SURFconext/CN=engine.surfconext.nl', $c->getName());
             $this->assertEquals('a36aac83b9a552b3dc724bfc0d7bba6283af5f8e', $c->getFingerprint());
+
+            $base64 = $c->toBase64();
+            $d = new CertParser($base64);
+            $this->assertEquals('a36aac83b9a552b3dc724bfc0d7bba6283af5f8e', $d->getFingerprint());
         }
     }
 
